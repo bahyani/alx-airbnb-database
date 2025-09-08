ae939cbfdb9b156a16479cf161c2e84cdc733217
bahyani <ikennaiwu@yahoo.com>
2025-09-08 02:40:46 +0100

    AIRBNB SQL QUERIES

---
+++ database-adv-script/README.md
@@ -0,0 +1,4 @@
+# SQL QUERIE TO SELECT DATA FROM USER AND BOOKING TABLE USING INNER JOIN
+
+1.**This is to retreive records of user
+2.** To retrieve records from the bookings having the same record as the user_id
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,11 @@
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM booking b
+INNER JOIN user u ON b.user_id = u.user_id;
